2dd93ebb8fb513a6358fdf451a7d34df4b4e68bf
Vincent Frias <buzofriasvicente@gmail.com>
2026-05-22 21:26:42 -0300

    fix a bug in pawn.class

--- xadrez/pieces/Pawn.java
+++ xadrez/pieces/Pawn.java
@@ -17,17 +17,7 @@ public class Pawn extends Piece {
 
         // Capturas diagonais: só valem se destino tiver inimigo.
         baseMovements.add(new Movement(1 * dir,  1, MoveType.CAPTURE_ONLY));
-        baseMovements.add(new Movement(1 * dir, -1, MoveType// movement_loop = false porque o rei tem alcance fixo (não desliza).
-        super(isWhite, false);
-
-        // 8 direções ao redor: combina dx, dy em {-1, 0, +1} e pula o (0,0)
-        // (o (0,0) seria "ficar parado", que não é um movimento válido).
-        for (int dx = -1; dx <= 1; dx++) {
-            for (int dy = -1; dy <= 1; dy++) {
-                if (dx == 0 && dy == 0) continue;
-                baseMovements.add(new Movement(dx, dy));
-            }
-        }.CAPTURE_ONLY));
+        baseMovements.add(new Movement(1 * dir, -1, MoveType.CAPTURE_ONLY));
 
         // Avanço duplo: 2 casas pra frente, mesma coluna. Só na primeira jogada
         // e (como todo avanço) só se destino vazio.
--- xadrez/pieces/Rook.java
+++ xadrez/pieces/Rook.java
@@ -4,15 +4,5 @@ import xadrez.Board;
 import xadrez.Move;
 
 public class Rook extends Piece {
-    public Rook(boolean isWhite) {
-        super(isWhite);
-    }
 
-    @Override
-    public boolean checkValidMove(Board board, Move move) {
-        if (move.fromLine != move.toLine && move.fromColumn != move.toColumn) {
-            return false;
-        }
-        return true;
-    }
 }
